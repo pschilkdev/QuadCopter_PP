7b2b7417f1d0c8d9b05859c52a3d83fdd099d089
pschilkdev <pschilk@stu.iics.k12.tr>
2015-12-28 23:01:31 +0200

    Added PPQCU_CU_MAIN Project and updated gitignore

---
+++ PPQCU_CU_MAIN.X/Main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   Main.c
+ * Author: Philipp
+ *
+ * Created on December 28, 2015, 10:47 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
