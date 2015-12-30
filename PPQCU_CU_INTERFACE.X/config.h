7f0ca310e25f7664c1b6ff38f53dd46c7e929b47
pschilkdev <pschilk@stu.iics.k12.tr>
2015-12-31 00:48:25 +0200

    Added intial config files, settings to be confirmed and untested

---
+++ PPQCU_CU_INTERFACE.X/config.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   config.h
+ * Author: Philipp
+ *
+ * Created on December 31, 2015, 12:29 AM
+ */
+
+#ifndef CONFIG_H
+#define	CONFIG_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* CONFIG_H */
+
