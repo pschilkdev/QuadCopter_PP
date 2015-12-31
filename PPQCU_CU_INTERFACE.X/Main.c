4bc4026725b6174d52d25c5a3fea771c5a6c3d30
pschilkdev <pschilk@stu.iics.k12.tr>
2015-12-31 14:51:00 +0200

    Created Initial Programm Structure for INTERFACE Chip

--- PPQCU_CU_INTERFACE.X/Main.c
+++ PPQCU_CU_INTERFACE.X/Main.c
@@ -9,14 +9,49 @@
 #include <stdlib.h>
 #include "config.h"
 
+//INIT
 void init_OSC();
+void init_INT();
+void init_I2C();
+
+//OPERATION
+void handshake();
+void read_data();
+void output(int value);
+void reset_mux();
+void timeout();
+
+//Servo Values
+int Serv1 = 0;
+int Serv2 = 0;
+int Serv3 = 0;
+int Serv4 = 0;
+int Serv5 = 0;
+int Serv6 = 0;
+
 
 /*
- * 
+ *  
  */
 int main(int argc, char** argv) {
-    
+
     init_OSC();
+    init_INT();
+    init_I2C();
+    
+    handshake();
+    
+    while(1){
+        read_data();
+        output(Serv1);
+        output(Serv2);
+        output(Serv3);
+        output(Serv4);
+        output(Serv5);
+        output(Serv6);
+        reset_mux();
+        timeout();
+    }
     
     return (EXIT_SUCCESS);
 }
@@ -29,6 +64,40 @@ void init_OSC() {
     OSCCONbits.SPLLEN = 0b1;
     //Clock determined by oscillator config fuses
     OSCCONbits.SCS = 0b00;
-
 }
 
+void init_INT() {
+    //Reset all Flags and disable all interrupts
+    INTCONbits.TMR0IF = 0b0;
+    INTCONbits.INTF = 0b0;
+    INTCONbits.IOCIF = 0b0;
+    PIE1 = 0b0;
+    PIE2 = 0b0;
+    PIR1 = 0b0;
+    PIR1 = 0b0;
+    
+    //General Initialization
+    INTCONbits.GIE = 0b1;
+    INTCONbits.PEIE = 0b1;
+    
+    //Timer0
+    
+    //Timer1
+    
+}
+
+void init_I2C(){
+    //TODO
+}
+
+void output(int value){
+    //TODO
+}
+
+void reset_mux(){
+    //TODO
+}
+
+void timeout(){
+    //TODO
+}
