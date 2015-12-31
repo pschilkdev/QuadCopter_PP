789ee1840d903bd935e9143dcec4d903fa786d30
pschilkdev <pschilk@stu.iics.k12.tr>
2015-12-31 03:14:11 +0200

    Oscillator Configuration. Untested...

--- PPQCU_CU_MAIN.X/config.h
+++ PPQCU_CU_MAIN.X/config.h
@@ -11,51 +11,47 @@
 #ifdef	__cplusplus
 extern "C" {
 #endif
-
-
-    // PIC32MX120F064H Configuration Bit Settings
-
-    // 'C' source line config statements
-
-#include <xc.h>
 #include <p32xxxx.h>
+#include <xc.h>
 
-    // DEVCFG3
-    // USERID = No Setting
+// DEVCFG3
+// USERID = No Setting
 #pragma config PMDL1WAY = OFF           // Peripheral Module Disable Configuration (Allow multiple reconfigurations)
 #pragma config IOL1WAY = OFF            // Peripheral Pin Select Configuration (Allow multiple reconfigurations)
 
-    // DEVCFG2
-#pragma config FPLLIDIV = DIV_12        // PLL Input Divider (12x Divider)
+// DEVCFG2
+#pragma config FPLLIDIV = DIV_2         // PLL Input Divider (2x Divider)
 #pragma config FPLLMUL = MUL_24         // PLL Multiplier (24x Multiplier)
-#pragma config FPLLODIV = DIV_256       // System PLL Output Clock Divider (PLL Divide by 256)
+#pragma config FPLLODIV = DIV_2         // System PLL Output Clock Divider (PLL Divide by 2)
 
-    // DEVCFG1
-#pragma config FNOSC = FRCDIV           // Oscillator Selection Bits (Fast RC Osc w/Div-by-N (FRCDIV))
+// DEVCFG1
+#pragma config FNOSC = FRCPLL           // Oscillator Selection Bits (Fast RC Osc with PLL)
 #pragma config FSOSCEN = OFF            // Secondary Oscillator Enable (Disabled)
-#pragma config IESO = ON                // Internal/External Switch Over (Enabled)
+#pragma config IESO = OFF               // Internal/External Switch Over (Disabled)
 #pragma config POSCMOD = HS             // Primary Oscillator Configuration (HS osc mode)
 #pragma config OSCIOFNC = OFF           // CLKO Output Signal Active on the OSCO Pin (Disabled)
-#pragma config FPBDIV = DIV_1           // Peripheral Clock Divisor (Pb_Clk is Sys_Clk/1)
+#pragma config FPBDIV = DIV_8           // Peripheral Clock Divisor (Pb_Clk is Sys_Clk/8)
 #pragma config FCKSM = CSDCMD           // Clock Switching and Monitor Selection (Clock Switch Disable, FSCM Disabled)
 #pragma config WDTPS = PS1048576        // Watchdog Timer Postscaler (1:1048576)
 #pragma config WINDIS = OFF             // Watchdog Timer Window Enable (Watchdog Timer is in Non-Window Mode)
 #pragma config FWDTEN = OFF             // Watchdog Timer Enable (WDT Disabled (SWDTEN Bit Controls))
 #pragma config FWDTWINSZ = WINSZ_25     // Watchdog Timer Window Size (Window Size is 25%)
 
-    // DEVCFG0
+// DEVCFG0
 #pragma config JTAGEN = OFF             // JTAG Enable (JTAG Disabled)
 #pragma config ICESEL = ICS_PGx1        // ICE/ICD Comm Channel Select (Communicate on PGEC1/PGED1)
 #pragma config PWP = OFF                // Program Flash Write Protect (Disable)
 #pragma config BWP = OFF                // Boot Flash Write Protect bit (Protection Disabled)
 #pragma config CP = OFF                 // Code Protect (Protection Disabled)
-
     /*
      * ======== GLOBAL OPTIONS ==========
      */
-
-#define F_OSC 50000000
-#define F_PER F_OSC/FPBDIV
+//System clock frequency
+#define F_OSC 48000000
+//Peripheral Clock Divider
+#define F_PER_DIV 8
+//Peripheral Clock frequency
+#define F_PER F_OSC/F_PER_DIV
 
 #ifdef	__cplusplus
 }
