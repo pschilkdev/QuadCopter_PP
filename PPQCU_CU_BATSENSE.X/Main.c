2553b5500daf1f88ff03db3b5b28e0079b487c33
pschilkdev <pschilk@stu.iics.k12.tr>
2016-01-02 06:01:26 +0200

    Added ADC code. Untested

--- PPQCU_CU_BATSENSE.X/Main.c
+++ PPQCU_CU_BATSENSE.X/Main.c
@@ -13,37 +13,44 @@ void init_OSC();
 void init_ADC();
 void init_I2C();
 void init_INT();
+void init_IO();
+
 void handshake();
 int checkBattery();
+int readADC();
 
 /*
  * 
  */
 
 int status;
+int thresh1;
+int thresh2;
 
 int main(int argc, char** argv) {
-    
+
     init_OSC();
     init_ADC();
     init_I2C();
     init_INT();
-    
+
     handshake();
-    
-    while(1){
+
+    while (1) {
         status = checkBattery();
-        
-        if(status == 0){
-            //Batter Ok
-        } if (status == 1){
+
+        if (status == 0) {
+            //Battery Ok
+        }
+        if (status == 1) {
             //Warn Stage 1
-        } if (status == 2){
+        }
+        if (status == 2) {
             //Warn Stage 2
         }
-        
+
     }
-    
+
     return (EXIT_SUCCESS);
 }
 
@@ -58,22 +65,58 @@ void init_OSC() {
 
 }
 
-void init_ADC(){
-    
+void init_ADC() {
+    ADCON0bits.CHS = 0b00011;
+    ADCON0bits.ADON = 0b1;
+    ADCON1bits.ADFM = 0b1;
+    ADCON1bits.ADCS = 0b110;
+    ADCON1bits.ADPREF = 0b00;
 }
 
-void init_I2C(){
-    
+void init_I2C() {
+
 }
 
-void init_INT(){
-    
+void init_INT() {
+
 }
 
-void handshake(){
-    
+void handshake() {
+    thresh1 = 500;
+    thresh2 = 400;
 }
 
-int checkBattery(){
-    
+int checkBattery() {
+    int v1 = readADC();
+    for (int i = 0; i < 15; i++) {
+        asm("nop");
+    }
+    int v2 = readADC();
+    for (int i = 0; i < 15; i++) {
+        asm("nop");
+    }
+    int v3 = readADC();
+    for (int i = 0; i < 15; i++) {
+        asm("nop");
+    }
+    int val = (v1+v2+v3)/4;
+
+    if (val <= thresh1) {
+        return 1;
+    } else if (val <= thresh2){
+        return 2;
+    } else {
+        return 0;
+    }
+}
+
+int readADC() {
+    for (int i = 0; i < 15; i++) {
+        asm("nop");
+    }
+    ADCON0bits.ADGO = 0b1;
+    while (ADCON0bits.ADGO) {
+        asm("nop");
+    }
+    return ((ADRESH << 8) | ADRESL);
 }
