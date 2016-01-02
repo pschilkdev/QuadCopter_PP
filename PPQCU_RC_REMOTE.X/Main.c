7f9183dd4016a61204b81a6f25141f06d8fccc18
pschilkdev <pschilk@stu.iics.k12.tr>
2016-01-02 03:41:42 +0200

    Created empty Remote Control Project

---
+++ PPQCU_RC_REMOTE.X/Main.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   Main.c
+ * Author: Philipp
+ *
+ * Created on January 2, 2016, 3:40 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <config.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ PPQCU_RC_REMOTE.X/config.h
@@ -0,0 +1,24 @@
+/* 
+ * File:   config.h
+ * Author: Philipp
+ *
+ * Created on January 2, 2016, 3:40 AM
+ */
+
+#ifndef CONFIG_H
+#define	CONFIG_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#include <xc.h>
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* CONFIG_H */
+
