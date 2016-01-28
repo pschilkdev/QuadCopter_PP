b7068e7d009a0b226c58d1caadcddf0fb85945ff
pschilkdev <pschilk@stu.iics.k12.tr>
2016-01-28 23:24:16 +0200

    Updated init_IO for BMI

--- PPQCU_CU_MAIN.X/Main.c
+++ PPQCU_CU_MAIN.X/Main.c
@@ -44,7 +44,7 @@ void init_IO() {
     //DEVCFG0bits.JTAGEN = 0;
     CFGCONbits.JTAGEN = 0;
     
-    //NRF SPI:
+    //Get Rid of Analog
     ANSELA = 0x0000;
     ANSELB = 0x0000;
     ANSELC = 0x0000;
@@ -53,25 +53,24 @@ void init_IO() {
     ANSELF = 0x0000;
     ANSELG = 0x0000;
     
-    TRISB = 0;
+    //NRF
     TRISBbits.TRISB9 = 1; //MISO
     SDI3Rbits.SDI3R = 0b0101;
-    
     TRISBbits.TRISB10 = 0; //MOSI
-    
-    //while(1){
-      //  LATBbits.LATB10 = 1;
-        //LATBbits.LATB10 = 0;
-    //}
-    
     RPB10Rbits.RPB10R = 0b1110;
-    
     TRISBbits.TRISB13 = 0; //CE
-    
     TRISBbits.TRISB14 = 0; //SCK
-    
     TRISBbits.TRISB15 = 0; //CSN
+    
+    //BMI
+    TRISGbits.TRISG7 = 1;//MISO
+    SDI2Rbits.SDI2R = 0b0001;
+    TRISGbits.TRISG8 = 0;//MOSI
+    RPG8Rbits.RPG8R = 0b0110;
+    TRISGbits.TRISG6 = 0;//SCK
+    TRISBbits.TRISB5 = 0;//CSB
 }
+
 
 void shake_NRF() {
     NRF_init(SPI3);
