46cd5aee5c84ee2e6779d2ed5bdabd2d8cba386b
pschilkdev <pschilk@stu.iics.k12.tr>
2016-02-08 14:34:59 +0200

    Added Quadcopter Config Files

---
+++ PPQCU_CU_MAIN.X/Quadcopter.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   Quadcopter.h
+ * Author: Philipp
+ *
+ * Created on February 8, 2016, 2:34 PM
+ */
+
+#ifndef QUADCOPTER_H
+#define	QUADCOPTER_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+#define IIC_ADR_MPL 0x60
+#define IIC_ADR_BAT 0x50
+#define IIC_ADR_RGB 0x40
+#define IIC_ADR_INT 0x20
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* QUADCOPTER_H */
+
