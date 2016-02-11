8b5cb36800097c11a23d1de4800a0632da6ef045
pschilkdev <pschilk@stu.iics.k12.tr>
2016-02-11 09:37:05 +0200

    Added intial files for MPL interface

---
+++ PPQCU_CU_MAIN.X/MPL.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   MPL.h
+ * Author: Philipp
+ *
+ * Created on February 6, 2016, 12:12 PM
+ */
+
+#ifndef MPL_H
+#define	MPL_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* MPL_H */
+
